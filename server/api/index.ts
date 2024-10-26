3e7777bdb62911aa2d2e3debce346c1402287afe
RawZ06 <longordo.alexandre@gmail.com>
2024-10-26 22:57:42 +0200

    debug

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,5 +1,19 @@
+import { exec } from 'child_process';
+
+export function lsPatches(): Promise<string> {
+  return new Promise(((resolve, reject) => exec(`ls /`, (error, stdout) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      reject(error);
+    }
+    // console.log(`stdout lsPatches: ${stdout}`);
+    // console.error(`stderr lsPatches: ${stderr}`);
+    resolve(stdout);
+  })));
+}
+
 export default defineEventHandler(async () => {
   return {
-    hello: "world",
+    ls: await lsPatches(),
   };
 });
